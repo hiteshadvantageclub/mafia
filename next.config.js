a8bee8b06b48f115a6f8e7b562fb116776c18b2b
hitesh sharma <hiteshsharma@workadvantage.in>
2025-05-29 21:39:49 +0530

    first commit 3

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        crypto: require.resolve('crypto-browserify'),
-      };
-    }
-    return config;
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
